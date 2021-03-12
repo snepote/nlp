3f9645777d3399059380a3ee4f02a6ee9a70fb8f
Sebastian Nepote <sebastian.nepote@helpling.com>
2021-03-12 14:16:37 +0100

    Twitter: print to CSV including sentiment score if provided

--- app/csv.rb
+++ app/csv.rb
@@ -7,11 +7,15 @@ query           = '$AMD'
 search_storage  = Twitter::SearchStorage.new(query: query)
 results         = search_storage.results.sort_by!{ |r| r[:id]}.reverse!
 if results.kind_of?(Array) && !results.empty?
+  language_storage = Language::Storage.new
   puts "id;created_at;name;followers_count;score"
   results.each do |tweet|
     tweet = JSON.parse(tweet.to_json.to_s, object_class: OpenStruct)
-    created_at = Time.parse(tweet.created_at).strftime('%d/%m/%Y %T')
-    # @todo: remove fake sentiment score
-    puts "#{tweet.id};#{created_at};#{tweet.user.name};#{tweet.user.followers_count};#{rand(-1.0..1.0)}"
+    sentiment = JSON.parse(language_storage.read(name: tweet.id).to_json.to_s, object_class: OpenStruct)
+    unless sentiment.nil?
+      score = sentiment.analyze_sentiment.documentSentiment.score || nil
+      created_at = Time.parse(tweet.created_at).strftime('%d/%m/%Y %T')
+      puts "#{tweet.id};#{created_at};#{tweet.user.name};#{tweet.user.followers_count};#{score}"
+    end
   end
 end
